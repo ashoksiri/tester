44e40c7b8854c8c75fa5de14fddf6ea993e5dc00
ashok <ashoksiri@yahoo.com>
2023-12-04 19:52:36 +0530

    Update abc.ts

--- abc.ts
+++ abc.ts
@@ -1,8 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {CapitalmarketsResp} from '@core/api/models';
 import {GlobalConstants, ITab} from '@core/constant/global-constants';
+import {CellClickedEvent, ColDef} from 'ag-grid-community';
+import {isNumber, omit,} from 'lodash';
+import {of} from 'rxjs';
 import {TemplateMonitorComponent} from "../../templates/template-monitor.component";
-import {CellClickedEvent, ColDef} from "ag-grid-community";
 
 @Component({
   selector: 'app-monitor',
@@ -10,52 +13,72 @@ import {CellClickedEvent, ColDef} from "ag-grid-community";
 })
 export class MonitorComponent extends TemplateMonitorComponent implements OnInit {
 
-  tabs: ITab[] = [GlobalConstants.tabs[0]]
-  filterTabs: ITab[] = GlobalConstants.tabs;
+  public data = {input: '', date: ''};
+  tabs: ITab[] = [GlobalConstants.capitaltabs[0]]
+  filterTabs: ITab[] = GlobalConstants.capitaltabs;
+  autoFillOptions: string[];
+
   filters = [
-    {key: 'facility', field: 'facility', header: 'Facility',},
+    {key: 'facilityID', field: 'facilityID', header: 'Facility',},
     {key: 'businessDate', field: 'businessDate', header: 'Business Date',},
-    {key: 'brokerCID', field: 'brokerData.broker_CID', header: 'Broker CID',},
+    {key: 'brokerCID', field: 'brokerCID', header: 'Broker CID',},
     {key: 'businessLine', field: 'business', header: 'Business',},
-    {key: 'nettingSetId', field: 'netting_Set_', header: 'Netting Set',},
-    {key: 'clientCID', field: 'client_CID', header: 'Client CID',},
+    {key: 'nettingSetId', field: 'nettingSet', header: 'Netting Set',}
   ];
 
-  initForm() {
-    this.searchForm = new FormGroup({
-      brokerAccountNumber: new FormControl(),
-      brokerCID: new FormControl(),
-      brokerName: new FormControl(),
-      businessDate: new FormControl(),
-      businessLine: new FormControl(),
-      clientCID: new FormControl(),
-      facility: new FormControl(),
-      fetchingLevel: new FormControl("FACILITY"),
-      nettingSetId: new FormControl(),
-      sftLevel: new FormControl("FL"),
-    });
-    this.searchForm.controls['businessDate'].setValue(this.businessDates[0]);
+  ngOnInit() {
+    this.initForm();
+    this.search();
+
   }
 
-  ngOnInit(): void {
-    this.api.businessDates$.subscribe((dates) => {
-      this.businessDates = dates;
-      this.initForm();
-    });
-  }
-
-  search(field = 'facility') {
-    console.log(1);
+  search(field = 'facilityID', initCall = true) {
     const cellOptions: ColDef = {
       onCellClicked: (event: CellClickedEvent) => {
         this.mycellClicked(event.colDef.field as string, event.value)
       },
       cellStyle: {cursor: 'pointer', textDecoration: 'underline', color: '#9b8c36'}
     }
-    const columnDefs = GlobalConstants.institutionalHeader.map((col: ColDef) => col.field === field ? ({...col, ...cellOptions}) : col);
+    const columnDefs = GlobalConstants.capitalMarketsMonitorColumnDefs.map((col: ColDef) => col.field === field ? ({...col, ...cellOptions}) : col);
     this.tabs[this.selectedTab].columnDefs = columnDefs;
-    this.tabs[this.selectedTab].rowData$ = this.api.getFacilities(this.searchForm.value);
-    this.dataFetched = true;
+
+    const payload = this.getPayload();
+    this.api.getCapitalMarkets(payload).subscribe((v: CapitalmarketsResp) => {
+      this.tabs[this.selectedTab].rowData$ = of(v.cpfePershingDataLst);
+      this.businessDates = v.businessDates;
+      this.autoFillOptions = v.brokerNames;
+      if (initCall) {
+        this.data.date = this.businessDates[0];
+      }
+      this.dataFetched = true;
+    });
+
+  }
+
+  initForm() {
+    this.searchForm = new FormGroup({
+      input: new FormControl(),
+      facilityID: new FormControl(),
+      businessDate: new FormControl(),
+      brokerCID: new FormControl(),
+      brokerName: new FormControl(),
+      businessLine: new FormControl(),
+      nettingSetId: new FormControl(),
+      sftLevel: new FormControl(),
+      entity: new FormControl('CAPITALMARKETS')
+    });
+  }
+
+  private getPayload() {
+    const payload = omit(this.searchForm.value, 'input');
+    if (this.data.input.length === 7 && isNumber(this.data.input)) {
+      payload.facilityID = this.data.input
+    } else if (this.data.input.length === 10 && isNumber(this.data.input)) {
+      payload.brokerCID = this.data.input;
+    } else {
+      payload.brokerName = this.data.input;
+    }
+    return payload;
   }
 
 }
