66a5a0f055d503dab31c4b7345fcfb94a8e521b8
ashok <ashoksiri@yahoo.com>
2023-12-04 19:44:38 +0530

    Update abc.ts

--- abc.ts
+++ abc.ts
@@ -1,130 +1,67 @@
-import {TemplateRef, ViewChild} from "@angular/core";
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {ColDef, GridOptions} from "ag-grid-community";
+import {CommonModule} from "@angular/common";
+import {SharedModule} from "@shared";
+import {Observable} from "rxjs";
 import {AgGridAngular} from "ag-grid-angular";
-import {ColDef, ColGroupDef, GridOptions, RowClassParams} from "ag-grid-community";
-import {GlobalConstants, ITab} from "@core/constant/global-constants";
-import {FormGroup} from "@angular/forms";
-import {APIService} from "@core/api/api.service";
-import {MatTabChangeEvent} from "@angular/material/tabs";
-import { Component } from '@angular/core';
-import {Observable, of} from "rxjs";
-import {UsageDataList, UsageResponse} from "@core/api/models";
-import {NgxSpinnerService} from "ngx-spinner";
-import {NgxRolesService} from "ngx-permissions";
 
-@Component({template: ''})
-export abstract class TemplateUsageComponent{
+@Component({
+  selector: 'app-template-grid',
+  standalone: true,
+  imports: [
+    CommonModule, SharedModule
+  ],
+  template: `
+    <div class="flex justify-between w-full pt-2 py-1">
+      <div class="w-2/6">
+        <mat-form-field appearance="outline" class="dense-3 w-full">
+          <mat-label>Search Table:-</mat-label>
+          <input id="filter-text-box" matInput (input)="onFilterTextBoxChanged()">
+        </mat-form-field>
+      </div>
+      <div class="flex justify-around">
+        <app-svg-icon icon="excel" [height]="35" [width]="35" (click)="exportOut('xls')"></app-svg-icon>
+        <app-svg-icon icon="pdf" [height]="35" [width]="35" (click)="exportOut('pdf')"></app-svg-icon>
+        <app-svg-icon [height]="35" [width]="35" (click)="exportOut('csv')"></app-svg-icon>
+      </div>
+    </div>
+    <div style="height:fit-content">
+      <ag-grid-angular class="ag-theme-alpine" style="width: 100%; height: 70%;" [rowData]="rowData | async"
+                       [columnDefs]="columnDefs" [gridOptions]="gridOptions" [defaultColDef]="defaultColDef">
+      </ag-grid-angular>
+    </div>
+  `,
+})
+export class TemplateGridComponent {
 
-  search = {initCall: true, entity: 'INSTITUTIONAL', cob: '19000101'};
-  businessDates!:Observable<string[]>;
-  gridOptions: GridOptions = {
-    ...GlobalConstants.gridOptions,
-    getRowStyle: (params: RowClassParams<UsageDataList>) => {
-      return { backgroundColor: params.data?.colorCode ?? ''}
-    },
-  };
-  defaultColDef: ColDef = {
-    headerClass: 'fit-header',
-    resizable: true,
-    wrapHeaderText: true,
-    autoHeaderHeight: true,
-    filter: 'agTextColumnFilter'
-  };
-  rowData !: Observable<UsageDataList[]>;
+  @ViewChild(AgGridAngular)
+  agGrid!: AgGridAngular;
 
-  columnDefs: (ColDef| ColGroupDef)[] = [...GlobalConstants.institutionalUsage];
+  @Input({required: true})
+  gridOptions: GridOptions;
 
-  constructor(public api:APIService, public spinner: NgxSpinnerService, public rolesService: NgxRolesService) {
-    console.log(this.rolesService.getRoles());
+  @Input({required: true})
+  defaultColDef: ColDef;
+
+  @Input({required: true})
+  columnDefs: ColDef[] | undefined
+
+  @Input({required: true})
+  rowData: Observable<any[]> | undefined
+
+  @Output()
+  export = new EventEmitter<string>();
+
+  @Output()
+  filterChanged = new EventEmitter();
+
+  exportOut(format: string) {
+    this.export.emit(format);
   }
 
-  refresh(init=false) {
-    this.spinner.show();
-    this.api.getUsages(this.search).subscribe((v: UsageResponse) => {
-      this.rowData = of(this.calculate(v.usageDataList));
-      this.businessDates = of(v.businessDates);
-      this.search.cob = init && v.businessDates ? v.businessDates[0] : this.search.cob;
-      this.spinner.hide();
-    });
+  onFilterTextBoxChanged() {
+    this.agGrid.api.setQuickFilter(
+      (document.getElementById('filter-text-box') as HTMLInputElement).value
+    );
   }
-
-
-numberWithCommas(x: number) {
-  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
-}
-
-  calculate(usageDataList: any[]) {
-
-    const usageList = usageDataList.map((usageData: any) => {
-      let notionalUsagePercentage = 0;
-      let cpfeUsagePercentage = 0;
-
-      if(usageData.notionalLimit != 0){
-        notionalUsagePercentage = (usageData.notionalAmount/usageData.notionalLimit)*100;
-      }else if(usageData.notionalAmount > 0){
-        // since limit is 0 this falls in the category of greater than 100%
-        notionalUsagePercentage = 101;
-      }
-
-      if(usageData.cpfeLimit != 0){
-        cpfeUsagePercentage = (usageData.cpfeAmount/usageData.cpfeLimit)*100;
-      }else if(usageData.cpfeAmount > 0){
-        // since limit is 0 this falls in the category of greater than 100%
-        cpfeUsagePercentage = 101;
-      }
-
-      // if usage is greater than equal to 100% then row color is red
-      if(notionalUsagePercentage >= GlobalConstants.RED_BEGIN_LIMIT || cpfeUsagePercentage >= GlobalConstants.RED_BEGIN_LIMIT){
-        usageData.colorCode = GlobalConstants.COLOR_CODE_RED;
-      }
-      // if usage is 90-99% then row color is yellow
-      else if((notionalUsagePercentage >= GlobalConstants.YELLOW_BEGIN_LIMIT && notionalUsagePercentage < GlobalConstants.RED_BEGIN_LIMIT) ||
-          (cpfeUsagePercentage >= GlobalConstants.YELLOW_BEGIN_LIMIT && cpfeUsagePercentage < GlobalConstants.RED_BEGIN_LIMIT)){
-        usageData.colorCode = GlobalConstants.COLOR_CODE_YELLOW;
-      }
-      // if usage is 75-89% then row color is orange
-      else if((notionalUsagePercentage >= GlobalConstants.ORANGE_BEGIN_LIMIT && notionalUsagePercentage < GlobalConstants.YELLOW_BEGIN_LIMIT) ||
-         (cpfeUsagePercentage >= GlobalConstants.ORANGE_BEGIN_LIMIT && cpfeUsagePercentage < GlobalConstants.YELLOW_BEGIN_LIMIT)){
-        usageData.colorCode = GlobalConstants.COLOR_CODE_ORANGE;
-      }
-      // usage is < 75% - no color
-      else{
-        usageData.colorCode = GlobalConstants.COLOR_CODE_DEFAULT;
-      }
-
-      // set the max(cpfe usage %, notional usage %)
-      if(notionalUsagePercentage > cpfeUsagePercentage){
-        usageData.usagePercentageForSorting = notionalUsagePercentage;
-      }else{
-        usageData.usagePercentageForSorting = cpfeUsagePercentage;
-      }
-
-      // divide to 1000 scale
-      usageData.notionalLimit = this.numberWithCommas(usageData.notionalLimit) //Math.round(usageData.notionalLimit*1000)/1000;
-      usageData.notionalAmount = this.numberWithCommas(usageData.notionalAmount) // Math.round(usageData.notionalAmount*1000)/1000;
-      usageData.notionalExcess = Math.round(usageData.notionalExcess*1000)/1000;
-      usageData.cpfeLimit = usageData.notionalAmount //Math.round(usageData.cpfeLimit*1000)/1000;
-      usageData.cpfeAmount = Math.round(usageData.cpfeAmount*1000)/1000;
-      usageData.cpfeExcess = Math.round(usageData.cpfeExcess*1000)/1000;
-      return usageData;
-    });
-    // return usageList;
-    usageList.sort((x, y) => {
-      const getCode = (v: any) =>
-        v.colorCode === GlobalConstants.COLOR_CODE_RED ? 3 :
-          v.colorCode === GlobalConstants.COLOR_CODE_YELLOW ? 2 :
-            v.colorCode === GlobalConstants.COLOR_CODE_ORANGE ? 1 : 0;
-      return getCode(x) - getCode(y);
-    });
-    return usageList.reverse();
-  }
-
-  ngOnInit(): void {
-    this.refresh(true);
-  }
-
-  export(format = 'csv') {
-    const dt = this.search.cob.split('/');
-    this.api.downloadUsageFile(`${dt[2]}${dt[0]}${dt[1]}`, format);
-  }
-
 }
